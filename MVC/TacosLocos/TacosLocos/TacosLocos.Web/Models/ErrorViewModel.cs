99927cb033b0246cdf1c4ab3995ee21fdf5070ac
macoAngelo <marco.angelo.blu@gmail.com>
2025-01-27 21:54:28 -0300

    Envio do projeto Taco Locos

---
+++ MVC/TacosLocos/TacosLocos/TacosLocos.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TacosLocos.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

e1de204278a7f8cc632db0ced8dd0d0a56438b64
macoAngelo <marco.angelo.blu@gmail.com>
2025-02-03 19:14:21 -0300

    ...

---
+++ MVC/MinhaPrimeiraAplicacaoWeb/MinhaPrimeiraAplicacao.Utils/Entidades/Marca.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinhaPrimeiraAplicacao.Utils.Entidades
+{
+    public class Marca : EntidadeBase
+    {
+        protected override string TableName => "MARCAS";
+    }
+}
